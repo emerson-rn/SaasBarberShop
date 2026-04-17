5c026a22db1debc48e1f3e47eb99f8194999a49d
Emerson Rdorigues Nunes <work.emerson.ti@gmail.com>
2026-04-17 10:19:49 -0300

    Correção: formulário personalizado para modelo de usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UsuarioCreationForm
 
 def cadastrar_usuario(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UsuarioCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             messages.success(request, f'Usuário {user.username} criado com sucesso!')
             return redirect('equipe')
     else:
-        form = UserCreationForm()
+        form = UsuarioCreationForm()
     
     return render(request, 'cadastro_usuario.html', {'form': form})
