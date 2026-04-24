004a259786bbd9bcf2c3737fd0a017043655a2ed
Emerson Rdorigues Nunes <work.emerson.ti@gmail.com>
2026-04-24 10:57:26 -0300

    ok

--- setup/urls.py
+++ setup/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from agendamentos import views
 from agendamentos.views import dashboard_barbeiro, lista_estoque, lista_equipe
 from usuarios import views as usuarios_views
@@ -23,6 +23,9 @@ from usuarios import views as usuarios_views
 urlpatterns = [
     # Página inicial (abre direto na dashboard)
     path('', dashboard_barbeiro, name='index'),
+    
+    # URLs de autenticação do Django
+    path('', include('django.contrib.auth.urls')),
     
     # Painel Administrativo do Django
     path('admin/', admin.site.urls),
@@ -33,6 +36,7 @@ urlpatterns = [
     path('equipe/', lista_equipe, name='equipe'),
     path('novo-agendamento/', views.novo_agendamento, name='novo_agendamento'),
     path('editar-agendamento/<int:pk>/', views.editar_agendamento, name='editar_agendamento'),
+    path('excluir-agendamento/<int:pk>/', views.excluir_agendamento, name='excluir_agendamento'),
     path('estoque/novo/', views.novo_produto, name='novo_produto'),
     path('estoque/editar/<int:pk>/', views.editar_produto, name='editar_produto'),
     path('estoque/deletar/<int:pk>/', views.deletar_produto, name='deletar_produto'),
@@ -41,6 +45,11 @@ urlpatterns = [
     path('historico/', views.historico_agendamentos, name='historico'),
     path('relatorios/', views.relatorios, name='relatorios'),
     path('agendamento/status/<int:pk>/', views.atualizar_status_agendamento, name='atualizar_status'),
+    path('agendamento/verificar-pendentes/', views.verificar_agendamentos_pendentes, name='verificar_pendentes'),
+    path('agendamento/confirmar/<int:pk>/', views.confirmar_chegada, name='confirmar_chegada'),
+    path('painel-admin/', views.painel_admin, name='painel_admin'),
+    path('meus-agendamentos/', views.portal_cliente, name='portal_cliente'),
+    path('portal-barbeiro/', views.portal_barbeiro, name='portal_barbeiro'),
     
     # Usuários
     path('novo-usuario/', usuarios_views.cadastrar_usuario, name='novo_usuario'),
